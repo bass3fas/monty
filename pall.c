6d45f9570687c1f08f7a5c0863b8d0e3e969c700
Bassant Aboelhassan <yaralikuslar64@gmail.com>
2023-10-19 01:04:15 -0700

    all

--- pall.c
+++ pall.c
@@ -18,24 +18,4 @@ void f_pall(stack_t **head, unsigned int counter)
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-}#include "monty.h"
-/**
- * f_pall - prints the stack
- * @head: stack head
- * @counter: no used
- * Return: no return
-*/
-void f_pall(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
-
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
 }
